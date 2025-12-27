4eb89a2afb17a826ce4cabd34bc562f5678d1068
guilhermesaverio <guilhermepereirasaverio2019@gmail.com>
2025-12-27 18:38:27 -0300

    Alterar e excluir
    Pratiquei Alteração e exclusão.

---
+++ AlterarExcluir.sql
@@ -0,0 +1,29 @@
+ --Adicionar uma nova coluna a uma tabela existente.
+ ALTER TABLE Livro
+ ADD Edição SMALLINT;
+
+ --Alterar o tipo de dados de uma coluna
+ ALTER TABLE Livro
+ ALTER COLUMN Edição TINYINT;
+
+ --Adicionar chave primaria
+ ALTER TABLE NOmeTabela
+ ADD PRIMARY KEY (Coluna);
+
+ --Excluir uma constraint de uma coluna
+ ALTER TABLE NomeTAbela
+ DROP CONSTRAINT NomeConstraint;
+
+ --Verificar nomes das contraints:
+ sp_help Livro;
+
+ --excluir uma coluna de uma tabela
+ ALTER TABLE Livro
+ DROP COLUMN Edição;
+
+ --Excluir uma tabela: DROP TABLE
+ DROP TABLE NomeTabela;
+
+ -- Renomear uma tabela: sp_rename
+ --sp_rename 'nome atual', 'novo nome';
+ sp_rename 'tbl_livros', 'Livro';
